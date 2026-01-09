70ccf6326c72d5daa47966b662ec4018e81fcd68
Anas Abdelhakim <anasabdoali22@gmail.com>
2026-01-09 14:04:00 +0200

    remove shadcn uis

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -11,18 +9,15 @@ import NotFound from "../pages/NotFound";
 import "./App.css";
 
 const App = () => (
-  <TooltipProvider>
-    <Sonner />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/plants" element={<ProductList />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  </TooltipProvider>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/plants" element={<ProductList />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/about" element={<AboutUs />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
